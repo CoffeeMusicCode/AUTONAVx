a1c4e585e63d13c4d56147a30062dce7048221f8
Jon Kiddy <jon.kiddy@gmail.com>
2015-05-08 12:24:05 -0400

    Answer for HW1.

--- Lecture 1 - Introduction/homework-1.py
+++ Lecture 1 - Introduction/homework-1.py
@@ -3,25 +3,15 @@ from math import sqrt
 
 def plan_mission(mission):
 
-    # this is an example illustrating the different motion commands,
-    # replace them with your own commands and activate all beacons
     commands  = [
-        cmd.down(0.5),
-        cmd.right(1),
-        cmd.turn_left(45),
-        cmd.forward(sqrt(2)),
-        cmd.turn_right(45),
-        cmd.right(1),
-        cmd.turn_left(45),
-        cmd.forward(sqrt(0.5)),
-        cmd.turn_left(90),
-        cmd.forward(sqrt(0.5)),
-        cmd.turn_left(45),
+        cmd.up(1),
         cmd.forward(1),
+        cmd.right(2),
+        cmd.forward(4),
+        cmd.left(4),
+        cmd.backward(4),
         cmd.turn_right(45),
-        cmd.backward(sqrt(2)),
-        cmd.turn_left(45),
-        cmd.forward(1),
+        cmd.forward(3),
     ]
 
     mission.add_commands(commands)
